a965c64296df719d0053b5d5ca47cfd3e8246414
optout <13562139+optout21@users.noreply.github.com>
2025-01-15 08:22:47 +0100

    Minor comment/TODO/typo changes (review)

--- lightning/src/ln/channelmanager.rs
+++ lightning/src/ln/channelmanager.rs
@@ -11519,6 +11519,7 @@ where
 			msg.channel_id)), counterparty_node_id);
 	}
 
+	/// TODO(splicing): Implement persisting
 	#[cfg(splicing)]
 	fn handle_splice_init(&self, counterparty_node_id: PublicKey, msg: &msgs::SpliceInit) {
 		let _persistence_guard = PersistenceNotifierGuard::optionally_notify(self, || {
@@ -11533,6 +11534,7 @@ where
 		});
 	}
 
+	/// TODO(splicing): Implement persisting
 	#[cfg(splicing)]
 	fn handle_splice_ack(&self, counterparty_node_id: PublicKey, msg: &msgs::SpliceAck) {
 		let _persistence_guard = PersistenceNotifierGuard::optionally_notify(self, || {
